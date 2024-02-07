bc8ed19cbef7b52ccb7568819c1be9f2081787d4
ternaksapi <yusufhaikaln7@gmail.com>
2024-02-07 18:21:14 +0700

    fixed typo

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
@@ -0,0 +1,9 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ProductTest {
+}
