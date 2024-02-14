a2f3366a43dc6e724df0f37492a8eedf5a43a33f
ternaksapi <yusufhaikaln7@gmail.com>
2024-02-14 19:02:42 +0700

    Added sonarcloud workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,17 @@
 plugins {
 	java
 	jacoco
+	id("org.sonarqube") version "4.4.1.3373"
 	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
+}
+
+sonar {
+	properties {
+		property("sonar.projectKey", "ternaksapi_eshop")
+		property("sonar.organization", "ternaksapi")
+		property("sonar.host.url", "https://sonarcloud.io")
+	}
 }
 
 group = "id.ac.ui.cs.advprog"
