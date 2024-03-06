4f658cb52c95c056217ec246b061e68dd9bd694d
ternaksapi <yusufhaikaln7@gmail.com>
2024-03-06 17:29:11 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,2 +1,43 @@
-package id.ac.ui.cs.advprog.eshop.model;public class Payment {
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Getter
+public class Payment {
+    String id;
+    String method;
+    String status;
+    Map<String, String> paymentData;
+
+    public Payment(String id, String method, Map<String, String> paymentData) {
+        this.id = id;
+        this.method = method;
+        this.paymentData = paymentData;
+        this.status = "PENDING";
+    }
+
+    public Payment(String id, String method, Map<String, String> paymentData, String status) {
+        this(id, method, paymentData);
+        this.setStatus(status);
+    }
+
+    public void setStatus(String status) {
+        if (status.equals("SUCCESS") || status.equals("REJECTED")) {
+            this.status = status;
+        } else if (status.equals("PENDING")) {
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+    public void setPaymentData(Map<String, String> paymentData) {
+        if (paymentData.isEmpty()) {
+            throw new IllegalArgumentException();
+        } else {
+            this.paymentData = paymentData;
+        }
+    }
 }
--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -1,2 +1,55 @@
-package id.ac.ui.cs.advprog.eshop.model;public class PaymentTest {
+package id.ac.ui.cs.advprog.eshop.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PaymentTest {
+    private Map<String, String> paymentData;
+
+    @BeforeEach
+    void setUp() {
+        this.paymentData = new HashMap<>();
+    }
+    @Test
+    void testCreatePaymentDefault(){
+        Payment payment = new Payment("13652556-012a-4c07-b546-54eb1396d79b",
+                "VOUCHER", this.paymentData);
+
+        assertEquals("13652556-012a-4c07-b546-54eb1396d79b", payment.getId());
+        assertEquals("VOUCHER", payment.getMethod());
+        assertEquals("REJECTED", payment.getStatus());
+        assertEquals(this.paymentData, payment.getPaymentData());
+    }
+    @Test
+    void testCreatePaymentSuccessStatus(){
+        Payment payment = new Payment("13652556-012a-4c07-b546-54eb1396d79b",
+                "VOUCHER", this.paymentData, "SUCCESS");
+
+        assertEquals("13652556-012a-4c07-b546-54eb1396d79b", payment.getId());
+        assertEquals("VOUCHER", payment.getMethod());
+        assertEquals("SUCCESS", payment.getStatus());
+        assertEquals(this.paymentData, payment.getPaymentData());
+    }
+    @Test
+    void testCreatePaymentInvalidStatus(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            Payment payment = new Payment("13652556-012a-4c07-b546-54eb1396d79b",
+                    "VOUCHER", this.paymentData, "MEOW");
+        });
+    }
+    @Test
+    void testSetPaymentToInvalidStatus(){
+        Payment payment = new Payment("13652556-012a-4c07-b546-54eb1396d79b",
+                "VOUCHER", this.paymentData);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            payment.setStatus("MEOW");
+        });
+    }
 }
+
