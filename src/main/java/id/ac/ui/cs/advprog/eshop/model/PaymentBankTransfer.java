14a82090ca29137251259a586b9ef427ff9fc11a
ternaksapi <yusufhaikaln7@gmail.com>
2024-03-06 20:00:10 +0700

    [GREEN] Implement PaymentBankTransfer model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentBankTransfer.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentBankTransfer.java
@@ -1,2 +1,26 @@
-package id.ac.ui.cs.advprog.eshop.model;public class PaymentBankTransfer {
+package id.ac.ui.cs.advprog.eshop.model;
+
+import enums.PaymentStatus;
+import java.util.Map;
+
+public class PaymentBankTransfer extends Payment {
+    public PaymentBankTransfer(String id, String method, Map<String, String> paymentData) {
+        super(id, method, paymentData);
+    }
+
+    public PaymentBankTransfer(String id, String method, Map<String, String> paymentData, String status) {
+        super(id, method, paymentData, status);
+    }
+
+    @Override
+    public void setPaymentData(Map<String, String> paymentData) {
+        if (paymentData.isEmpty() || paymentData.get("bankName").isEmpty()) {
+            this.status = PaymentStatus.REJECTED.getValue();
+        } else if (paymentData.get("referenceCode").isEmpty())
+            this.status = PaymentStatus.REJECTED.getValue();
+        else {
+            this.paymentData = paymentData;
+            this.status = PaymentStatus.SUCCESS.getValue();
+        }
+    }
 }
